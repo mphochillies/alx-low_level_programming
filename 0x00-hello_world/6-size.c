b6f69b89001f2b7a3efed48214de59002c0fed17
mphochillies <mphooza2@gmail.com>
2023-07-13 09:00:45 -0700

    Hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+/**
+ * main - A sizes of variables
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of int: %lu byte(s)\n", sizeof(b));
+	printf("Size of long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of float: %lu byte(s)\n", sizeof(e));
+	return (0);
+}
 
