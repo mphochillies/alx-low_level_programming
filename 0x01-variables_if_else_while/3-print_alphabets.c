b2764efe003b85aad33a44f0846e2e61159bd89e
mphochillies <mphooza2@gmail.com>
2023-07-14 03:22:12 -0700

    Bets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= '2') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= '2') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
