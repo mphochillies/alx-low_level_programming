f6ebd402bb83d7e39ed9c671a716272619c280c0
mphochillies <mphooza2@gmail.com>
2023-07-14 03:08:33 -0700

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ * return
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			    putchar(c);
+			    c++;
+		}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Variables is else while
