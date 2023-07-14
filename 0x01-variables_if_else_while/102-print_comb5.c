96caa4380c507dfa2228b9e0f87aa6489c8288f7
mphochillies <mphooza2@gmail.com>
2023-07-14 07:50:08 -0700

    Bet

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,25 +6,23 @@
  */
 int main(void)
 {
-	int i, j;
+	int num1, num2;
 
-	for (j = 0; j < 100; j++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (j = 0; j < 100; j++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
